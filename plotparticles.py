d7cfe499b8079c8078cbe90641db787ad86953b3
JoeBeton <joe_beton@hotmail.co.uk>
2019-11-27 12:05:00 +0000

    Restructured files such that plotparticles, superparticles and unifyparticles are all modules which contain relevant functions. This renders plot_eulerXY_pdf.py and filament_functions.py redundant - will probably delete these later

---
+++ plotparticles.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot
+from matplotlib.backends.backend_pdf import PdfPages
+
+import parse_star
+
+def plot_changes():
+    pass
+
+def plot_filament_pdf():
+    pass
