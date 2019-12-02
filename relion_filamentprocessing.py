a6a050fc5863139dc9b7dd6739040bfe79ba5d0f
JoeBeton <joe_beton@hotmail.co.uk>
2019-12-02 11:46:51 +0000

    added remove_shortfils parser function

--- relion_filamentprocessing.py
+++ relion_filamentprocessing.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import argparse
 
-from filtools import unifyparticles, superparticles, plotparticles
+from filtools import unifyparticles, plotparticles
 
 parser = argparse.ArgumentParser()
 
@@ -13,6 +13,7 @@ parser.add_argument('--unify_rot', '--rot', type = float, help = 'Unify the rot 
 parser.add_argument('--unify_psi', '--psi', action = 'store_true', help = 'Unify the psi values for filaments')
 
 parser.add_argument('--reset_tilt', action = 'store_true', help = 'Option to fit the tilt values for each filament')
+parser.add_argument('--remove_shortfils', type = int, help = 'Option to remove the particles from filaments shorter than the stated value')
 
 parser.add_argument('--make_superparticles', type = int, metavar = '*Specify windowing range*', help = 'Option to generate superparticles')
 
@@ -27,6 +28,10 @@ do_plots = args.plot_changes
 if args.reset_tilt:
     unifyparticles.reset_tilt(args.input, do_plots)
 
+if args.remove_shortfils:
+    unifyparticles.remove_shortFilaments(args.input, args.remove_shortfils)
+
+
 if args.unify_tilt:
     unifyparticles.unify_tilt(args.input, do_plots)
 
@@ -36,8 +41,12 @@ if args.unify_rot:
 if args.unify_psi:
     unifyparticles.unify_psi(args.input, do_plots)
 
+
 if args.make_superparticles:
+    from filtools import superparticles
+
     superparticles.make_superparticles(args.input, args.make_superparticles)
+
 
 if args.plot_pdf:
     plotparticles.plot_filament_pdf(args.input)
