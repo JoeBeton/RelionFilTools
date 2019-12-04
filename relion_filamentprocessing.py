7f20687f8a7ed5df4b08c4d0e032ad337cfab90d
JoeBeton <joe_beton@hotmail.co.uk>
2019-12-04 15:06:26 +0000

    fixed fibretwist movie parser options

--- relion_filamentprocessing.py
+++ relion_filamentprocessing.py
@@ -21,7 +21,7 @@ parser.add_argument('--make_superparticles', type = int, metavar = '*Specify win
 
 parser.add_argument('--plot_changes', action = 'store_true', help = 'Option to save pdf plots showing the old and updated angles for each filament')
 parser.add_argument('--plot_pdf', '--p', action = 'store_true', help = 'Plot the particle data from filaments into a pdf file')
-parser.add_argument('--make_fibretwist_movie', action = 'store_true')
+parser.add_argument('--make_fibretwist_movie', nargs = 2, metavar = '[frame rate] [length of video (seconds)]', help = 'Make a movie showing a twisting fibre')
 
 args=parser.parse_args()
 
@@ -57,4 +57,6 @@ if args.plot_pdf:
     plotparticles.plot_filament_pdf(args.input)
 
 if args.make_fibretwist_movie:
-    plotparticles.makeTurningFilamentVideo(args.input, 60)
+    from filtools import superparticles
+
+    superparticles.makeTurningFilamentVideo(args.input, int(args.make_fibretwist_movie[0]), int(args.make_fibretwist_movie[1]))
