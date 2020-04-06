e49f82b360ac57938727c9ad3e00a165bc8e2f1f
JoeBeton <joe_beton@hotmail.co.uk>
2020-04-06 10:36:03 +0100

    plot pdf now only saves 100 plots per star file

--- filtools/plotparticles.py
+++ filtools/plotparticles.py
@@ -11,46 +11,47 @@ def plot_filament_pdf(starfile_path):
 
     filament_data = parse_star.readFilamentsFromStarFile(starfile_path)
 
+    step = int(filament_data.number_of_filaments/100)
+
+    if step < 1:
+        step = 1
+
     #Code to plot the graphs as a pdf file
     with PdfPages(starfile_path[:-5] + '_alignmentplot.pdf') as pdf:
-    	for i in range(filament_data.number_of_filaments):
+        for i in range(0, filament_data.number_of_filaments, step):
+            phi=filament_data.getNumpyFilamentColumn(i, 'rlnAngleRot')
+            the=filament_data.getNumpyFilamentColumn(i, 'rlnAngleTilt')
+            psi=filament_data.getNumpyFilamentColumn(i, 'rlnAnglePsi')
+            xsh=filament_data.getNumpyFilamentColumn(i, 'rlnOriginXAngst')
+            ysh=filament_data.getNumpyFilamentColumn(i, 'rlnOriginYAngst')
+            tracklength = filament_data.getNumpyFilamentColumn(i, 'rlnHelicalTrackLengthAngst')
 
-    		#If statement only plots one in every 50 "tubes" to prevent making an insanely large pdf file
-    		if i % 50 == 1:
+            f,(ax1,ax2,ax3,ax4)=plt.subplots(1,4,figsize=(12,6))
+            plt.setp([ax1,ax2,ax3,ax4],xticks=[i for i in range(1,len(phi)+1,2)],xlabel='Particle Number')
 
-    			phi=filament_data.getNumpyFilamentColumn(i, 'rlnAngleRot')
-    			the=filament_data.getNumpyFilamentColumn(i, 'rlnAngleTilt')
-    			psi=filament_data.getNumpyFilamentColumn(i, 'rlnAnglePsi')
-    			xsh=filament_data.getNumpyFilamentColumn(i, 'rlnOriginXAngst')
-    			ysh=filament_data.getNumpyFilamentColumn(i, 'rlnOriginYAngst')
-    			tracklength = filament_data.getNumpyFilamentColumn(i, 'rlnHelicalTrackLengthAngst')
+            ax1.plot(tracklength, phi, 'o')
+            ax1.set_ylim([-181,181])
+            ax1.set_yticks([i for i in range(-180,180+1,40)])
+            ax1.set_ylabel('Phi Angle')
+            ax1.set_title('Phi')
 
-    			f,(ax1,ax2,ax3,ax4)=plt.subplots(1,4,figsize=(12,6))
-    			plt.setp([ax1,ax2,ax3,ax4],xticks=[i for i in range(1,len(phi)+1,2)],xlabel='Particle Number')
+            ax2.plot(tracklength, the, 'o')
+            ax2.set_yticks([i for i in range(-180,180+1,40)])
+            ax2.set_ylim([-181,181])
+            ax2.set_ylabel('Theta Angle')
+            ax2.set_title('Theta')
 
-    			ax1.plot(tracklength, phi, 'o')
-    			ax1.set_ylim([-181,181])
-    			ax1.set_yticks([i for i in range(-180,180+1,40)])
-    			ax1.set_ylabel('Phi Angle')
-    			ax1.set_title('Phi')
+            ax3.plot(tracklength, psi, 'o')
+            ax3.set_yticks([i for i in range(-180,180+1,40)])
+            ax3.set_ylim([-181,181])
+            ax3.set_ylabel('Psi Angle')
+            ax3.set_title('Psi')
 
-    			ax2.plot(tracklength, the, 'o')
-    			ax2.set_yticks([i for i in range(-180,180+1,40)])
-    			ax2.set_ylim([-181,181])
-    			ax2.set_ylabel('Theta Angle')
-    			ax2.set_title('Theta')
+            ax4.plot(tracklength, xsh, 'o',label='Xshift')
+            ax4.plot(tracklength, ysh, 'o',label='Yshift')
+            ax4.set_title('X/Y Shifts')
+            ax4.set_ylabel('Shift (pixels)')
+            ax4.legend(loc='upper right')
 
-    			ax3.plot(tracklength, psi, 'o')
-    			ax3.set_yticks([i for i in range(-180,180+1,40)])
-    			ax3.set_ylim([-181,181])
-    			ax3.set_ylabel('Psi Angle')
-    			ax3.set_title('Psi')
-
-    			ax4.plot(tracklength, xsh, 'o',label='Xshift')
-    			ax4.plot(tracklength, ysh, 'o',label='Yshift')
-    			ax4.set_title('X/Y Shifts')
-    			ax4.set_ylabel('Shift (pixels)')
-    			ax4.legend(loc='upper right')
-
-    			pdf.savefig()
-    			plt.close()
+            pdf.savefig()
+            plt.close()
