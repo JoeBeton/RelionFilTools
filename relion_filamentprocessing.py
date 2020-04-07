410cb046bac14695511dce6c520d45fd7b701e10
JoeBeton <joe_beton@hotmail.co.uk>
2020-04-07 15:22:18 +0100

    Changed the input method, all functions can now handle multiple starfiles which will be processed iteratively

--- relion_filamentprocessing.py
+++ relion_filamentprocessing.py
@@ -6,7 +6,7 @@ from filtools import unifyparticles, plotparticles
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--input', '--i', required = True, type= str, help = 'Input the path to a RELION starfile' )
+parser.add_argument('--input', '--i', nargs = '+', help = 'Input star file(s)')
 
 parser.add_argument('--unify_tilt', '--tilt', action = 'store_true', help = 'Unify the tilt values for filaments')
 parser.add_argument('--unify_rot', '--rot', type = float, help = 'Unify the rot values for filaments')
@@ -22,46 +22,52 @@ parser.add_argument('--make_superparticles', type = int, metavar = '*Specify win
 parser.add_argument('--plot_changes', action = 'store_true', help = 'Option to save pdf plots showing the old and updated angles for each filament')
 parser.add_argument('--plot_pdf', '--p', action = 'store_true', help = 'Plot the particle data from filaments into a pdf file')
 parser.add_argument('--plot_fillenhist', action = 'store_true', help = 'Plot a histogram of the filament lengths')
-
-parser.add_argument('--fibretwist_movie', nargs = 2, metavar = '[frame rate] [length of video (seconds)]', help = 'Make a movie showing a twisting fibre')
+parser.add_argument('--compare_starfiles', action = 'store_true', help = 'Plot a histogram of the filament lengths')
 
 args=parser.parse_args()
+
 
 do_plots = args.plot_changes
 
 if args.reset_tilt:
-    unifyparticles.reset_tilt(args.input, do_plots)
+    for starfile in args.input:
+        unifyparticles.reset_tilt(starfile, do_plots)
 
 if args.remove_shortfils:
-    unifyparticles.remove_shortFilaments(args.input, args.remove_shortfils)
+    for starfile in args.input:
+        unifyparticles.remove_shortFilaments(starfile, args.remove_shortfils)
 
 if args.select_angles:
-    unifyparticles.selectParticlesbyAlignmentAngleRange(args.input, args.select_angles[0], int(args.select_angles[1]), int(args.select_angles[2]))
-
+    for starfile in args.input:
+        unifyparticles.selectParticlesbyAlignmentAngleRange(starfile, args.select_angles[0], int(args.select_angles[1]), int(args.select_angles[2]))
 
 if args.unify_tilt:
-    unifyparticles.unify_tilt(args.input, do_plots)
+    for starfile in args.input:
+        unifyparticles.unify_tilt(starfile, do_plots)
 
 if args.unify_rot:
-    unifyparticles.unify_rot(args.input, args.unify_rot, plot_changes = do_plots)
+    for starfile in args.input:
+        unifyparticles.unify_rot(starfile, args.unify_rot, plot_changes = do_plots)
 
 if args.unify_psi:
-    unifyparticles.unify_psi(args.input, do_plots)
+    for starfile in args.input:
+        unifyparticles.unify_psi(starfile, do_plots)
 
 
 if args.make_superparticles:
     from filtools import superparticles
-
-    superparticles.make_superparticles(args.input, args.make_superparticles)
+    for starfile in args.input:
+        superparticles.make_superparticles(starfile, args.make_superparticles)
 
 
 if args.plot_pdf:
-    plotparticles.plot_filament_pdf(args.input)
+    for starfile in args.input:
+        plotparticles.plot_filament_pdf(starfile)
 if args.plot_fillenhist:
-    plotparticles.plotFilamentLengthHistogram(args.input)
-
-
-if args.fibretwist_movie:
-    from filtools import superparticles
-
-    superparticles.makeTurningFilamentVideo(args.input, int(args.fibretwist_movie[0]), int(args.fibretwist_movie[1]))
+    for starfile in args.input:
+        plotparticles.plotFilamentLengthHistogram(starfile)
+if args.compare_starfiles:
+    if len(args.input) != 2:
+        quit('Please provide two starfiles for this function')
+    else:
+        plotparticles.compareFilamentNumbers(args.input[0], args.input[1])
