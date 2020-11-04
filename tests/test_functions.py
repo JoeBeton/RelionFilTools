c8777f7b8d6f0679c3cf9cd7052658743655625e
JoeBeton <joe_beton@hotmail.co.uk>
2020-11-04 16:35:23 +0000

    added tests - so far only for new function

---
+++ tests/test_functions.py
@@ -0,0 +1,26 @@
+from filtools import(
+            unifyparticles,
+            plotparticles,
+            get_helixinimodel2d_angles,
+            parse_star
+            )
+
+import numpy as np
+
+
+test_starfile1 = 'tests/test_data/test_star1.star'
+test_starfile2 = 'tests/test_data/test_star2.star'
+
+class TestFunctions:
+
+    def test_updateAlignments(self):
+
+        unifyparticles.updateAlignments(test_starfile1, test_starfile2)
+        saved_star_path = 'tests/test_data/test_star1_updatedAlignments.star'
+        saved_star = parse_star.readBlockDataFromStarfile(saved_star_path)
+        updating_angles_star = parse_star.readBlockDataFromStarfile(test_starfile2)
+
+        rot_saved_file = saved_star.getNumpyDataColumn('rlnAngleRot')
+        rot_orig_file = updating_angles_star.getNumpyDataColumn('rlnAngleRot')
+
+        assert np.isclose(rot_saved_file, rot_orig_file).all()
